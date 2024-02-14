b94f436e88c4b8bc3a65c5760160c523c4ceeca1
Moataz <moatazw72@gmail.com>
2024-02-14 23:22:26 +0200

    adding a function to validate the date input

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
 function calculateAge(){
 
-const date=new Date();
+const today=new Date();
 const birthdayInput= document.getElementById("birthday").value;
-const result=document.querySelector("#res-item")
+const result=document.querySelector("#result");
+const resultContainer=document.querySelector(".result-container");
 const arr=birthdayInput.split("-");
 
 const day=arr[0];
 const month=arr[1]-1;
 const year=arr[2];
-console.log(arr)
 const birthDate=new Date(year, month, day);
-console.log(birthDate);
+// console.log(birthDate);
 
 
+
+const isValidDate = (date)=>{
+    
+    return(
+        !(Object.prototype.toString.call(date).value === Date) && !isNaN(date)
+        )
+       
+    
+}
+
+
+
+   if(!isValidDate(birthDate)){
+
+    alert("Please enter a valid date")
+    return
+   }
 
 
 
 
 
 }
+
+
+
 
 
 
